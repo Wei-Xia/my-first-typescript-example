adf2472f1308d52eea430416b5077e3a37088bce
Wei Xia <xw900812@gmail.com>
2017-12-21 13:47:17 -0500

    dsafds

--- module.js
+++ module.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var moduleCir_1 = require("./module/moduleCir");
+var moduleRec_1 = require("./module/moduleRec");
 console.log("module", moduleCir_1.PI);
 console.log("module", moduleCir_1.calcCircumference(284));
+console.log("module", moduleRec_1.calcRectangle(10, 22));
--- module.ts
+++ module.ts
@@ -1,4 +1,6 @@
 import {PI, calcCircumference} from "./module/moduleCir";
+import {calcRectangle} from "./module/moduleRec";
 
 console.log("module", PI);
 console.log("module", calcCircumference(284));
+console.log("module", calcRectangle(10, 22));
