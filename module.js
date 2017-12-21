70ec9910f21865334694ea5ea4c15b0cdb43eac2
Wei Xia <xw900812@gmail.com>
2017-12-21 13:55:54 -0500

    21321312

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var moduleCir_1 = require("./module/moduleCir");
+var Circle = require("./module/moduleCir");
 var moduleRec_1 = require("./module/moduleRec");
-console.log("module", moduleCir_1.PI);
-console.log("module", moduleCir_1.calcCircumference(284));
+console.log("module", Circle.PI);
+console.log("module", Circle.calcCircumference(284));
 console.log("module", moduleRec_1.calcRectangle(10, 22));
--- module.ts
+++ module.ts
@@ -1,6 +1,6 @@
-import {PI, calcCircumference} from "./module/moduleCir";
+import * as Circle from "./module/moduleCir";
 import {calcRectangle} from "./module/moduleRec";
 
-console.log("module", PI);
-console.log("module", calcCircumference(284));
+console.log("module", Circle.PI);
+console.log("module", Circle.calcCircumference(284));
 console.log("module", calcRectangle(10, 22));
