f3f8fb1949133630631779b1831f50effea4ab71
Wei Xia <xw900812@gmail.com>
2017-12-26 16:05:49 -0500

    11

---
+++ ReactJS/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {Home} from "./components/Home";
+
+ReactDOM.render(<Home name="wei", age={28}/>, document.getElementById("app"));
